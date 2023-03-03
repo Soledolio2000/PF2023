642ef1c94ee5dc46305076f9d7e8448803331791
Grullon-2000 <117415011+Grullon-2000@users.noreply.github.com>
2023-03-03 18:49:54 -0400

    .

--- Server/Models/Factura.cs
+++
@@ -1,7 +0,0 @@
-public class Factura
-{
-    public int Id { get; set; }
-    public int IdReserva { get; set; }
-    public DateTime FechaEmision { get; set; }
-    public double TotalAPagar { get; set; }
-}
